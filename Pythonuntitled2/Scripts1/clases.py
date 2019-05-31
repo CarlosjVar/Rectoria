023a885146a7f1dc3241eb6c6499ecd60b1c99d4
TilapiaKIddo2 <jostenoriop@gmail.com>
2019-05-31 03:09:20 -0600

    Subí las clases
    f in the chat

---
+++ Pythonuntitled2/Scripts1/clases.py
@@ -0,0 +1,122 @@
+class Persona():
+    "Atributos"
+    cedula=0
+    nombreCompleto=""
+    telefono=0
+    voto=0
+    
+    "Médotos"
+    def __init__(self,ced,nC,tel):
+        self.cedula=ced
+        self.nombreCompleto=nC
+        self.telefono=tel
+        return
+
+    def setVoto (self,vot):
+        self.voto=vot
+        return
+
+    def getVoto (self):
+        return self.voto
+
+    def getTelefono (self):
+        return self.telefono
+
+    def getCedula (self):
+        return self.cedula
+    
+    def getnombreCompleto (self):
+        return self.nombreCompleto
+
+class Estudiante (Persona):
+    "Atributos"
+    carnet=""
+    carrera=""
+    tipo=""
+
+    "Metodos"
+    def __init__(self,ced,nC,tel,crnt):
+        self.carnet=crnt
+        self.tipo="estudiante"
+        Persona.__init__(self,ced,nC,tel)
+
+    def setCarrera(self,carre):
+        self.carrera=carre
+        return
+
+    def getCarrera (self,carre):
+        return self.carrera
+
+    def getTipo (self):
+        return self.tipo
+
+    def getCarnet(self):
+        return self.carnet
+
+class Profesor(Persona):
+    "Atributos"
+    publicaciones=""
+    candidato=""
+    tipo=""
+    
+    "Métodos"
+    def __init__(self,ced,nC,tel,pub):
+        self.publicaciones=pub
+        self.tipo="profesor"
+        Persona.__init__(self,ced,nC,tel)
+
+    def setCandidato(self,cand):
+        self.candidato=cand
+        return
+    
+    def getCandidato (self):
+        return self.candidato
+
+    def getPublicaciones(self):
+        return self.publicaciones
+
+    def getTipo (self):
+        return self.tipo
+
+class Administrativo(Persona):
+    "Atributos"
+    puesto=""
+    extension=""
+    tipo=""
+
+    "Métodos"
+    def __init__(self,ced,nC,tel,pst,ext):
+        self.puesto=pst
+        self.extension=ext
+        self.tipo="administrativo"
+        Persona.__init__(self,ced,nC,tel)
+
+    def getExtension(self):
+        return self.extension
+
+    def getPuesto(self):
+        return self.puesto
+
+    def getTipo (self):
+        return self.tipo
+
+    
+
+    
+
+    
+    
+    
+        
+
+    
+    
+
+
+
+
+
+
+    
+    
+    
